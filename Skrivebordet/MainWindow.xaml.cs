9ba92083d95fa357ba2d2d5e384d011819dce393
JulieSøndergaard-Hilleke <jsh@itklyngen.dk>
2022-06-21 12:58:58 +0200

    it works :-)

--- Skrivebordet/MainWindow.xaml.cs
+++ Skrivebordet/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Timers;
+using System.Diagnostics;
 
 namespace Skrivebordet
 {
@@ -22,7 +23,7 @@ namespace Skrivebordet
     /// </summary>
     public partial class MainWindow : Window
     {
-        List<string> url = new List<string>();
+        List<string> urls = new List<string>();
         public const int SPI_SETDESKWALLPAPER = 20;
         public const int SPIF_UPDATEINIFILE = 1;
         public const int SPIF_SENDCHANGE = 2;
@@ -38,35 +39,60 @@ namespace Skrivebordet
         {
             Timer timer = new Timer(time);
             timer.Elapsed += ChangeWallpaper;
+            timer.AutoReset = true;
+            timer.Start();
+            
         }
 
         private void ChangeWallpaper(object sender, ElapsedEventArgs e)
         {
-            SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, url[index], SPIF_UPDATEINIFILE | SPIF_SENDCHANGE);
+            if (index < urls.Count - 1)
+            {
+                index++;
+            } else
+            {
+                index = 0;
+            }
+
+            SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, urls[index], SPIF_UPDATEINIFILE | SPIF_SENDCHANGE);
+
+            
         }
 
         private void buttonDelete_Click(object sender, RoutedEventArgs e)
         {
-            url.Remove(textBoxUrl.Text);
+            urls.Remove(textBoxUrl.Text);
+            textBoxUrl.Text = "";
+            foreach (string tb in urls)
+            {
+                Debug.WriteLine(tb);
+            }
         }
 
         private void buttonUpload_Click(object sender, RoutedEventArgs e)
         {
-            url.Add(textBoxUrl.Text);
+            urls.Add(textBoxUrl.Text);
+            textBoxUrl.Text = "";
+            foreach (string tb in urls)
+            {
+                Debug.WriteLine(tb);
+            }
+            
         }
 
         private void buttonHour_Click(object sender, RoutedEventArgs e)
         {
-
+            SetTimer(10000);    
         }
 
         private void buttonDay_Click(object sender, RoutedEventArgs e)
         {
-
+            SetTimer(86400000);
         }
 
         private void buttonWeek_Click(object sender, RoutedEventArgs e)
         {
+            SetTimer(604800000);
 
         }
     }
